ef634cca0121f83722db4215cc34f811aa78e150
Rajdeep <rajdipgupta019@gmail>
2021-11-26 16:05:42 +0530

    Head Tag and Some Global CSS Added

---
+++ pages/document.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+function Document() {
+  return (
+    <div>
+      <Head>
+        <title>Booklee</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="https://www.linkpicture.com/q/Booklee-LOGO_2.png" type="image/x-icon"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"></link>
+      </Head>
+    </div>
+  )
+}
+
+export default Document; 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Document from './document'
 
 export default function Home() {
   return (
     <>
+    <Document />
     <h1>Booklee Initialized</h1>
+    <h2>This is H2</h2>
     <h3>Book description</h3>
+    <button>Click Me</button>
     </>
   )
 }

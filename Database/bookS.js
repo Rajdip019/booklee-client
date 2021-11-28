c7c45061dd3a1e1f079ecca6e53c142c5159dc6c
Rajdeep <rajdipgupta019@gmail>
2021-11-29 02:15:11 +0530

    Product Display Cards Initialized

---
+++ Database/bookS.js
@@ -0,0 +1,104 @@
+const BookS = [
+    {
+        _id: 1,
+        name: "Atomic Habits",
+        author: "Yang Lee",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx1dvD6I9WaCRlb9BOPudHwC32EaKI47lbEw&usqp=CAU",
+        condition: 5,
+        category: "Self-growth",
+        Price: 198,
+        seller_id: 1
+    },
+    {
+        _id: 2,
+        name: "Rich Dad Poor Dad",
+        author: "Robert T",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK4NRduF2lgMLW5fcPZwFmuPoX7bg8hhVO-g&usqp=CAU",
+        condition: 4,
+        category: "Self-growth",
+        Price: 55,
+        seller_id: 1
+    },
+    {
+        _id: 3,
+        name: "Hero Alam",
+        author: "Me Loo",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGqzMq76XTP4phqNVlZ15tsWHH0DzW0QVteA&usqp=CAU",
+        condition: 5,
+        category: "Self-growth",
+        Price: 21,
+        seller_id: 4
+    },
+    {
+        _id: 4,
+        name: "Cat lovers",
+        author: "Meow",
+        photo: "https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+        condition: 5,
+        category: "Love",
+        Price: 201,
+        seller_id: 4
+    },
+    {
+        _id: 5,
+        name: "Dog Love",
+        author: "Me Loouter",
+        photo: "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        condition: 4,
+        category: "Love",
+        Price: 2100,
+        seller_id: 2
+    }
+    ,  {
+        _id: 6,
+        name: "Self care",
+        author: "Robert joe",
+        photo: "https://images.unsplash.com/photo-1607983513311-fbb47e096b9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2VsZiUyMGNhcmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        condition: 4,
+        category: "Self-care",
+        Price: 2001,
+        seller_id: 3
+    }
+    ,  {
+        _id: 7,
+        name: "Heath is Weath",
+        author: "Harman Singh",
+        photo: "https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGVhbHRofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        condition: 3,
+        category: "Health",
+        Price: 121,
+        seller_id: 3
+    },
+    {
+        _id: 8,
+        name: "Horror Night",
+        author: "Kenedy Biswa",
+        photo: "https://images.unsplash.com/photo-1601513445506-2ab0d4fb4229?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aG9ycm9yfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        condition: 5,
+        category: "Story",
+        Price: 1121,
+        seller_id: 2
+    },{
+        _id: 9,
+        name: "Batul the Great",
+        author: "Ranjan Dhar",
+        photo: "https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVzY2xlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        condition: 2,
+        category: "Comic",
+        Price: 21,
+        seller_id: 4
+    },
+    {
+        _id: 10,
+        name: "Tintin",
+        author: "Arghe",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuh9In07hpKDIXZMwDcE1WLekWbN9ldHVuMA&usqp=CAU",
+        condition: 5,
+        category: "Comic",
+        Price: 210,
+        seller_id: 5
+    }
+
+]
+
+export default BookS

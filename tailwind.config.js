e7b8d2565a1c8a79204f6ffd531530a9fc91e99f
Rajdeep <rajdipgupta019@gmail>
2021-11-28 20:53:12 +0530

    Booklee Initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,37 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin:{
+          darkGreen: 'var(--dark-green)',
+          darkBlue: 'var(--dark-blue)',
+          darkRed: 'var(--dark-red)',
+          lightGreen: 'var(--light-green)',
+          lightBlue: 'var(--light-blue)',
+          lightRed: 'var(--light-red)'
+        }
+      },
+      backgroundColor: {
+        skin:{
+          darkGreen: 'var(--dark-green)',
+          darkBlue: 'var(--dark-blue)',
+          darkRed: 'var(--dark-red)',
+          lightGreen: 'var(--light-green)',
+          lightBlue: 'var(--light-blue)',
+          lightRed: 'var(--light-red)',
+          hoverGreen: 'var(--btn-hover-green)',
+          hoverBlue: 'var(--btn-hover-blue)'
+        },
+      },
+      fontFamily: {
+        rokkitt: "'Rokkitt', serif;",
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio"),require('@tailwindcss/forms')]
+  
 }

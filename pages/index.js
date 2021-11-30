d4c7c0781d8514c9a0dd2757354cc49cd7c5faa7
Rajdeep <rajdipgupta019@gmail>
2021-11-30 17:41:41 +0530

    Modals and Popups Added

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import react from "react"
 import Document from "./document"
-import Modals from "./Components/modal"
+import MobileVerifyPopUp from "./Components/MobileVerifyPopUp"
 
-function UserdetailsEducational(user){
-  return(
-  <UserProfileOthers
-  name={user.Name}
-  img={user.Photo}
-  location={user.Location}
-  />
-  )
-}
+
+// function UserdetailsEducational(user){
+//   return(
+//   <UserProfileOthers
+//   name={user.Name}
+//   img={user.Photo}
+//   location={user.Location}
+//   />
+//   )
+// }
 
 export default function Home() {
   return (
     <>  
     <Document />
-    <Modals />
+    <MobileVerifyPopUp
+    name="Rajdeep Sengupta" 
+    img="https://bit.ly/dan-abramov"  
+    />
     </>
   
   )

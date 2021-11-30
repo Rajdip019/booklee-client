6bb78f7b8aa7652080e33a8f64d2e20bee29742c
Rajdeep <rajdipgupta019@gmail>
2021-12-01 00:12:52 +0530

    Product Details Added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import react from "react"
 import Document from "./document"
-import MobileVerifyPopUp from "./Components/MobileVerifyPopUp"
+import ProductDetailsSell from "./Components/ProductDetailsSell"
+import ProductDetailsDonation from "./Components/ProductDetailsDonation"
 
 
-// function UserdetailsEducational(user){
+// function bookDetails(book){
 //   return(
 //   <UserProfileOthers
-//   name={user.Name}
-//   img={user.Photo}
-//   location={user.Location}
+//   name={book.Name}
+//   img={book.Photo}
+//   location={book.Location}
 //   />
 //   )
 // }
@@ -17,10 +18,26 @@ export default function Home() {
   return (
     <>  
     <Document />
-    <MobileVerifyPopUp
-    name="Rajdeep Sengupta" 
-    img="https://bit.ly/dan-abramov"  
+    <ProductDetailsSell
+    name="Atomic Habits"
+    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx1dvD6I9WaCRlb9BOPudHwC32EaKI47lbEw&usqp=CAU"
+    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in"
+    category="Self Growth"
+    author="John Doe"
+    condition="4.5"
+    adress="Belghoria, Kolkata"
+    price="315"
     />
+    <ProductDetailsDonation
+    name="H.C Verma"
+    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs8YHRXZuC55v8s2BLHzbaYZUc2fBnzTbpkQ&usqp=CAU"
+    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in"
+    author="John Doe"
+    condition="4.6"
+    adress="Belghoria, Kolkata"
+
+     />
+    
     </>
   
   )

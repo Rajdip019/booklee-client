3231d9419a328ae0d160536aa684a1603c5efd80
Debajyoti <debajyotisaha14@gmail.com>
2021-12-06 03:31:59 +0530

    Form updated

--- pages/ListBookForDonating/index.js
+++ pages/ListBookForDonating/index.js
@@ -14,6 +14,7 @@ const ListBookForDonating = () => {
     <Document />
     <Navbar />
     <form>
+
       <GeneralSidebar title="List Your Book" />
       <div className="ml-[0px] lg:ml-[300px] lg:w-[calc(100%-300px)] ">
       <div className="hidden lg:block bg-skin-lightGreen text-skin-darkGreen rounded-xl w-11/12 mx-auto mt-5 shadow-lg">
@@ -133,11 +134,12 @@ const ListBookForDonating = () => {
       </div>
       </div>
 
-    </div>
-
-
     </form>
     </div>
+
+
+
+
 
   );
 };
